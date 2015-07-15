cd59778f138bfac64f23855a60149ed6022ed924
Ann Kim <aumk21@gmail.com>
2015-07-15 16:37:01 -0600

    Second

--- animations.js
+++ animations.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-	$('#tweet-controls').css({'display': 'none'})
+	// Step 2
+	$('.tweet-compose').on('click', function() {
+		$('.tweet-compose').css({'width': '100%', 'height': '5em'}, 300);
+		$('#tweet-controls').css('display', 'block');
+	});
+
+
+	// Step 3 - decrease character count as type
+	$('.tweet-compose').keydown(function() {
+		var left = 140 - $(this).val().length;
+		$('#char-count').text(left);
+	});
+
+
+	//Step 3 - character count turns red when 10 or less characters left
+	$('.tweet-compose').on('keyup', function() {
+		var text = $('.tweet-compose').val();
+		var charLeft = 140 - text.length;
+		console.log(charLeft);
+		$('#char-count').html(charLeft);
+		if (charLeft < 11) {
+			$('#char-count').css('color', 'red');
+		} if (charLeft > 10) {
+			$('#char-count').css('color', '#999');
+		}
+	});
+
+	// Step 4 - Disable 'Tweet' button when more than 140 characters
+	
 });
